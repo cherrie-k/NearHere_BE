f6492c3f5e7e3b4494f3e2ff2677f40d22f0f3d3
cherrie-k <cherriekim925@gmail.com>
2024-07-10 04:08:06 +0900

    Initialize Dart Project

---
+++ bin/herenow_backend.dart
@@ -0,0 +1,5 @@
+import 'package:herenow_backend/herenow_backend.dart' as herenow_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${herenow_backend.calculate()}!');
+}
---
+++ test/herenow_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:herenow_backend/herenow_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
